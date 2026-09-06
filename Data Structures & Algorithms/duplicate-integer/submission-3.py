2edf4e493547c6cc3072c4eeb9ff279136d2572c
Ayush Baghel <sweayushcodes@gmail.com>
2026-09-06 19:50:21 +0530

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        # list can be empty
+        # couple of ways of solving this problem
+        # 1. set -> add all elements and then return when you find an existing element in the set
+        # 2. frequency map {num : count} - overkill 
+        # 3. AI optimised - len(Counter(nums)) == len(nums)
+        # 4. sort and compare adjancent elements 
+
+        return len(Counter(nums)) != len(nums)
+
+
+        
